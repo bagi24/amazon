9ba0e1cc4b4d9659077b719d2e95dab32a40c2fd
bagi24 <bagrat.injgia24@gmail.com>
2025-02-12 20:53:36 +0400

    initial state,install Material-UI, Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwAYsVbgiqYaLk-xiKYLBTqVQef8PufR4',
+  authDomain: 'fir-b193a.firebaseapp.com',
+  projectId: 'fir-b193a',
+  storageBucket: 'fir-b193a.firebasestorage.app',
+  messagingSenderId: '581496915003',
+  appId: '1:581496915003:web:851e42f019378e091c881e',
+  measurementId: 'G-W91BS0LCWX',
+};
